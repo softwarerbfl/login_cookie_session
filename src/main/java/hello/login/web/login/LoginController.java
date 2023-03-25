b50c65c5ab8b7b57b8a94b985ede9dc691ad58a3
softwarerbfl <32202018@dankook.ac.kr>
2023-03-25 16:44:13 +0900

    로그인 처리하기 - 쿠키 사용

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -24,21 +26,42 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult result){
-        if (result.hasErrors()){
+    public String login(@Valid @ModelAttribute LoginForm form,
+                        BindingResult result,
+                        HttpServletResponse response) {
+        if (result.hasErrors()) {
             return "login/loginForm";
         }
         Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
 
         // 로그인 과정을 거치다가 오류가 난 경우
         // 즉, 아이디에 대한 비번이 맞지 않은 경우
-        if (loginMember==null){
+        if (loginMember == null) {
             result.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 -> 홈 화면으로 redirect
+        //로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        //cookie의 값은 항상 String 으로 들어가야 한다.
+        Cookie cookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(cookie);
+
         return "redirect:/";
+
+    }
+
+    //로그아웃 로직 추가
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+    private void expireCookie(HttpServletResponse response, String cookieName){
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 쿠키의 종료날짜를 0으로 지정
+        response.addCookie(cookie);
 
     }
 }
